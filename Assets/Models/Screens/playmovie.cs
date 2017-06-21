d66d287d07bf5501ae2093b05fe71aaba18cf5bf
chrisedhog <chris.edhogben@gmail.com>
2017-06-21 19:38:10 +1000

    First push

---
+++ Assets/Models/Screens/playmovie.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playmovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        movie.Play();
+        movie.loop = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //    Renderer r = GetComponent<Renderer>();
+        //    MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+        //    if (movie.isPlaying)
+        //    {
+        //    }
+        //    else
+        //    {
+        //        movie.Play();
+        //    }
+
+
+	}
+}
